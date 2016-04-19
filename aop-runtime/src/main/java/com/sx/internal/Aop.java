688d5c1f861af29d39a2a68fcd16732db0a9cb65
贺冰 <heb@huaxindata.com.cn>
2016-04-19 16:02:18 +0800

    init

---
+++ aop-runtime/src/main/java/com/sx/internal/Aop.java
@@ -0,0 +1,52 @@
+package com.sx.internal;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.CodeSignature;
+
+@Aspect
+public class Aop {
+    private static volatile boolean enabled = true;
+
+    @Pointcut("within(@com.sx.annotation.Aop *)")
+    public void withinAnnotatedClass() {
+    }
+
+    @Pointcut("execution(!synthetic * *(..)) && withinAnnotatedClass()")
+    public void methodInsideAnnotatedType() {
+    }
+
+    @Pointcut("execution(@com.sx.annotation.Aop * *(..)) || methodInsideAnnotatedType()")
+    public void method() {
+    }
+
+    @Around("method()")
+    public Object inject(ProceedingJoinPoint joinPoint) throws Throwable {
+        if (!enabled) return joinPoint.proceed();
+
+        CodeSignature codeSignature = (CodeSignature) joinPoint.getSignature();
+
+        Class<?> cls = codeSignature.getDeclaringType();
+        String methodName = codeSignature.getName();
+        Object[] parameterValues = joinPoint.getArgs();
+
+        AopManager.Inject inject = AopManager.getInject(cls);
+
+        Object result;
+
+        if (inject == null || !inject.before(methodName)) {
+            result = joinPoint.proceed();
+        } else {
+            result = inject.process(methodName, parameterValues);
+        }
+
+        if (inject == null) {
+            return result;
+        } else {
+            return inject.after(methodName, result);
+        }
+    }
+
+}
---
+++ aop-runtime/src/main/java/com/sx/internal/AopManager.java
@@ -0,0 +1,47 @@
+package com.sx.internal;
+
+
+import com.sx.internal.map.ArrayMap;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hebing on 2016/4/19.
+ */
+public class AopManager {
+
+    private static ArrayMap<Class, Inject> injects = new ArrayMap<>();
+
+    public interface Inject {
+        /**
+         * if return true,will not call old method
+         *
+         * @param methodName
+         * @return
+         */
+        public boolean before(String methodName);
+
+        /**
+         * if before reutrn true,you need to do this
+         *
+         * @param methodName
+         * @param params
+         * @return
+         */
+        public Object process(String methodName, Object... params);
+
+        /**
+         * @param result the result will be return
+         * @return
+         */
+        public Object after(String methodName, Object result);
+    }
+
+    public static void addInject(Class<?> clazz, Inject inject) {
+        injects.put(clazz, inject);
+    }
+
+    static Inject getInject(Class<?> clazz) {
+        return injects.get(clazz);
+    }
+}
